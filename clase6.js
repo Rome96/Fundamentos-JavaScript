bc519095c17694addf5042d75e558382750e3c11
Turiano Romero <romero.guzman18@gmail.com>
2018-11-19 12:28:09 -0500

    update

--- clase6.js
+++ clase6.js
@@ -1,10 +1,16 @@
-var persona = {
+var turi = {
     nombre: 'Turiano',
     apellido: 'Romero',
     edad: 22,
     pais: 'Colombia'
 }
 
+var isaac = {
+    nombre: 'Isaac',
+    apellido: 'Ortiz',
+    edad: 10,
+    pais: 'Colombia'
+}
 
 function imprimirNombreEnMayusculas(n){
     n = n.toUpperCase()
