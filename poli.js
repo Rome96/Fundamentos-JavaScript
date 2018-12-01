d86e1409e37b9a58fa229ca2c2861e4b8f730bcb
Turiano Romero <romero.guzman18@gmail.com>
2018-12-01 02:02:20 -0500

    reto de palabras palindromos :neckbeard:

---
+++ poli.js
@@ -0,0 +1,13 @@
+const palabra = prompt("Escribe tu palabra")
+
+function polindromo(p){
+
+  let  pa = p.split("").reverse() // .split("") -> convierte al string a un array
+
+    if (pa.join("") === p ){ // .join("") -> convierte al array en un string
+        alert('palindromo')
+    }else{
+        alert('no es palindromo')
+    } 
+}
+polindromo(palabra)
