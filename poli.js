bb05769b034846527cc5fc3af2144b8a44e779bf
Turiano Romero <romero.guzman18@gmail.com>
2018-12-06 11:19:01 -0500

    update

--- poli.js
+++ poli.js
@@ -4,10 +4,11 @@ function polindromo(p){
 
   let  pa = p.split("").reverse() // .split("") -> convierte al string a un array
 
-    if (pa.join("") === p ){ // .join("") -> convierte al array en un string
+    if (pa.join() === p ){ // .join("") -> convierte al array en un string
         alert('palindromo')
     }else{
         alert('no es palindromo')
     } 
 }
 polindromo(palabra)
+
