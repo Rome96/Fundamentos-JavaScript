447a9370ccc36eda91fc201db78f318399f5a7b6
Turiano Romero <romero.guzman18@gmail.com>
2019-01-09 20:12:02 -0500

    operador this

---
+++ Funciones En Js/9-operador-this.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const boton = document.querySelector('.boton')
+
+boton.addEventListener('click', function(){
+    console.log(this.innerHTML)
+   // alert('Lorem ipsum dolo corporis iusto voluptas fugiat quis minus illo nobis sunt corrupti ve')
+})
