24e387acba7bda29c642226ccc44586dac3df215
Turiano Romero <romero.guzman18@gmail.com>
2019-01-09 23:18:54 -0500

    eventos del teclado

---
+++ Eventos en JavaScript/2-eventosDelTeclado.js
@@ -0,0 +1,18 @@
+'use strict'
+
+//keydown
+//keypress
+//keyup
+
+
+window.addEventListener('keydown', function(){
+    console.log('Pulsando Tecla')
+})
+
+window.addEventListener('keypress', function(){
+    console.log('Tecla Pulsada')
+})
+
+window.addEventListener('keyup', function(){
+    console.log('Tecla Liberada')
+})
